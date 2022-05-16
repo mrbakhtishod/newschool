b6dd5bd8925bba3b56f9710f043ea92c36087e7f
programmer0409 <programmer0409@gmail.com>
2022-05-16 09:01:26 +0500

    student page and teacher page

--- resources/views/site/students/students.blade.php
+++ resources/views/site/students/students.blade.php
@@ -4,6 +4,121 @@
 
 @section('content')
 <div class="container">
-   <div style="width: 100%; height: 100vh; background-color: #f1f1ff;"></div>
+   <section class="s5">
+            <div class="student-container">
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-3 img-student">
+                    <img src="/image/student.png" alt="">
+                  </div>
+                  <div class="col-md-8 info-student">
+                    <h2>Ismi Familiya Sharifovich</h2>
+                    <p class="bio-text">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, id iste ullam nisi ad aspernatur iure eum? At ullam expedita atque voluptatibus, recusandae consequuntur perferendis corporis quo quaerat dolorum incidunt nam, omnis quidem mollitia dignissimos quos error maxime ipsa eos reprehenderit. Iure hic iste sint ad beatae molestias, veritatis nemo!
+                    </p>
+                    <h3>Erishgan yutuqlari</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, debitis.</p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint quisquam vitae impedit, praesentium consequuntur minima.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit hic, officiis quisquam necessitatibus corporis dolore nemo ab sed vitae neque consequuntur, est iste quam inventore?</p>
+                  </div>
+                  <div class="col-md-1 networking-student">
+                    <div class="networks">
+                      <a href="#" class="telegram"><i class="fa-solid fa-paper-plane"></i></a>
+                      <a href="#" class="fasebook"><i class="fa-brands fa-facebook"></i></a>
+                      <a href="#" class="instagram"><i class="fa-brands fa-instagram-square"></i></a>
+                      <a href="#" class="e-pochta"><i class="fa-solid fa-envelope"></i></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section class="s6">
+            <div class="students-container">
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-4">
+                    <div class="box1">
+                      <a href="#">
+                        <div class="image">
+                          <img src="image/students1.jpeg" alt="">
+                        </div>
+                        <div class="info-text">
+                          <h4>Ismi Familiya Sharfi</h4>
+                          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis eaque exercitationem optio quos nemo, odit saepe temporibus consectetur veniam quae nulla obcaecati voluptatibus tempora sit mollitia, rerum provident. Necessitatibus, eaque?</p>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="box1">
+                      <a href="#">
+                        <div class="image">
+                          <img src="image/students1.jpeg" alt="">
+                        </div>
+                        <div class="info-text">
+                          <h4>Ismi Familiya Sharfi</h4>
+                          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis eaque exercitationem optio quos nemo, odit saepe temporibus consectetur veniam quae nulla obcaecati voluptatibus tempora sit mollitia, rerum provident. Necessitatibus, eaque?</p>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="box1">
+                      <a href="#">
+                        <div class="image">
+                          <img src="image/students1.jpeg" alt="">
+                        </div>
+                        <div class="info-text">
+                          <h4>Ismi Familiya Sharfi</h4>
+                          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis eaque exercitationem optio quos nemo, odit saepe temporibus consectetur veniam quae nulla obcaecati voluptatibus tempora sit mollitia, rerum provident. Necessitatibus, eaque?</p>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="box1">
+                      <a href="#">
+                        <div class="image">
+                          <img src="image/students1.jpeg" alt="">
+                        </div>
+                        <div class="info-text">
+                          <h4>Ismi Familiya Sharfi</h4>
+                          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis eaque exercitationem optio quos nemo, odit saepe temporibus consectetur veniam quae nulla obcaecati voluptatibus tempora sit mollitia, rerum provident. Necessitatibus, eaque?</p>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="box1">
+                      <a href="#">
+                        <div class="image">
+                          <img src="image/students1.jpeg" alt="">
+                        </div>
+                        <div class="info-text">
+                          <h4>Ismi Familiya Sharfi</h4>
+                          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis eaque exercitationem optio quos nemo, odit saepe temporibus consectetur veniam quae nulla obcaecati voluptatibus tempora sit mollitia, rerum provident. Necessitatibus, eaque?</p>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="box1">
+                      <a href="#">
+                        <div class="image">
+                          <img src="image/students1.jpeg" alt="">
+                        </div>
+                        <div class="info-text">
+                          <h4>Ismi Familiya Sharfi</h4>
+                          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis eaque exercitationem optio quos nemo, odit saepe temporibus consectetur veniam quae nulla obcaecati voluptatibus tempora sit mollitia, rerum provident. Necessitatibus, eaque?</p>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
 </div>
 @endsection
--- resources/views/site/teachers/teachers.blade.php
+++ resources/views/site/teachers/teachers.blade.php
@@ -3,7 +3,113 @@
 @section('title', 'Teachers')
 
 @section('content')
-<div class="container">
-   <div style="width: 100%; height: 100vh; background-color: #f1f1f1;"></div>
-</div>
+   <div class="container">
+      <section class="s7">
+
+         <div class="teacher-container">
+            <div class="container">
+               <div class="row">
+
+                  <div class="col-md-9">
+                     <div class="teacher-content">
+                        <h2>Boymurodov Adham Xushimqulovish</h2>
+                        <div class="teacher-call box">
+
+                           <div class="teacher-image">
+                              <img src="assets/image/adham_boymurodov_img.png" alt="">
+                           </div>
+                          
+                           <div class="e-pochta box">
+                              <a href="#">
+                                 <a href="#" class="e-pochta"><i class="fa-solid fa-envelope"></i></a>
+                              </a>
+                              <a href="#">boymurodovadham09@gmail.com</a>
+                           </div>
+
+                           <div class="tel-nomer box">
+                              <a href="#">
+                                <i class="fas fa-phone-square-alt"></i>
+                              </a>
+                              <span>+99899-009-80-71</span>
+                           </div>
+
+                           <div class="fasebook box">
+                              <a href="#" class="fasebook"><i class="fa-brands fa-facebook"></i></a>
+                              <span>adham_boymurodov</span>
+                           </div>
+
+                           <div class="telegram box">
+                              <a href="#">
+                                 <i class="fa-solid fa-paper-plane"></i>
+                              </a>
+                           </div>
+
+                        </div>
+                        <img src="assets/image/Adham_Boymurodov.jpg" alt="Asosiy rasm">
+                     </div>
+                  </div>
+
+                  <div class="col-md-3">
+                     <div class="all-teachers">
+                        <h2>Professior <br> O'qituvchilar</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos iure repudiandae animi eius hic quo labore neque similique in eligendi.</p>
+                        <div class="teacher-box">
+                           <a href="#">
+                              <div class="image">
+                                 <img src="assets/image/students1.jpeg" alt="">
+                                 <h3>Familayeva Ismina Otasini qizi</h3>
+                                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur, corrupti.</p>
+                              </div>
+                           </a>
+                        </div>
+                        <hr>
+                        <div class="teacher-box">
+                           <a href="#">
+                              <div class="image">
+                                 <img src="assets/image/students1.jpeg" alt="">
+                                 <h3>Familayeva Ismina Otasini qizi</h3>
+                                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur, corrupti.</p>
+                              </div>
+                           </a>
+                        </div>
+                        <hr>
+                        <div class="teacher-box">
+                           <a href="#">
+                              <div class="image">
+                                 <img src="assets/image/students1.jpeg" alt="">
+                                 <h3>Familayeva Ismina Otasini qizi</h3>
+                                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur, corrupti.</p>
+                              </div>
+                           </a>
+                        </div>
+                        <hr>
+                        <div class="teacher-box">
+                           <a href="#">
+                              <div class="image">
+                                 <img src="assets/image/students1.jpeg" alt="">
+                                 <h3>Familayeva Ismina Otasini qizi</h3>
+                                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur, corrupti.</p>
+                              </div>
+                           </a>
+                        </div>
+                        <hr>
+                        <div class="teacher-box">
+                           <a href="#">
+                              <div class="image">
+                                 <img src="assets/image/students1.jpeg" alt="">
+                                 <h3>Familayeva Ismina Otasini qizi</h3>
+                                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur, corrupti.</p>
+                              </div>
+                           </a>
+                        </div>
+                        <hr>
+                     </div>
+                  </div>
+
+               </div>
+            </div>
+         </div>
+
+      </section>
+   </div>
 @endsection
